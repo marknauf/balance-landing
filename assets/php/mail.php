37facf0db4fd99c75fe957a4b0fe44aa3961b662
MarkNauf <mnaufel@asu.edu>
2021-03-12 11:14:15 -0700

    website launch update

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -20,7 +20,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "hridoy1272@gmail.com";
+        $recipient = "mark.naufel@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
